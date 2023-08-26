ec5d1e2fe4796f815ccabb97cda4726d6943520b
analuizaff <nanaluff@hotmail.com>
2023-08-26 17:50:35 -0300

    adicionei o incremento de pontos

--- src/Main.hs
+++ src/Main.hs
@@ -21,20 +21,26 @@ initialState :: StdGen -> GameState
 initialState g = (Player 0, [], g, 0, Running, 0)
 
 render :: GameState -> Picture
-render (player, obstacles, _, _, status) = 
+render (player, obstacles, _, _, status, points) =
     case status of
-        Running -> pictures $ playerPic : map obstaclePic obstacles
-        GameOver -> scale 0.3 0.3 $ translate (-400) 0 $ color red $ text "Game Over"
+        Running ->
+            pictures [scorePic, playerPic, obstaclesPic]
+        GameOver ->
+            pictures [scorePic, gameOverPic]
   where
+    scorePic = translate (-windowWidth / 2 + 10) (windowWidth / 2 - 20) $ scale 0.2 0.2 $ color white $ text $ "Score: " ++ show points
     playerPic = translate (playerPosition player) (-340) $ color red $ rectangleSolid 50 50
     obstaclePic obs = translate (obstaclePosition obs) (obstacleY obs) $ color blue $ rectangleSolid 50 100
+    obstaclesPic = pictures $ map obstaclePic obstacles
+    gameOverPic = scale 0.3 0.3 $ translate (-400) 0 $ color red $ text "Game Over"
+
 
 update :: Float -> GameState -> GameState
-update t (player, obstacles, gen, time, status) 
-    | status == GameOver = (player, obstacles, gen, time, GameOver)
-    | any (collides player) obstacles = (player, obstacles, gen, time, GameOver)
-    | time > 1  = (player, newObstacle : movedObstacles, newGen, 0, Running)
-    | otherwise = (player, movedObstacles, gen, time + t, Running)
+update t (player, obstacles, gen, time, status, points) 
+    | status == GameOver = (player, obstacles, gen, time, GameOver, points)
+    | any (collides player) obstacles = (player, obstacles, gen, time, GameOver, points)
+    | time > 1  = (player, newObstacle : movedObstacles, newGen, 0, Running, points + 1)
+    | otherwise = (player, movedObstacles, gen, time + t, Running, points)
   where
     movedObstacles = filter (\o -> obstacleY o > -400) $ map moveObstacle obstacles
     moveObstacle obs = obs { obstacleY = obstacleY obs - obstacleSpeed }
@@ -43,15 +49,16 @@ update t (player, obstacles, gen, time, status)
     newObstacle = Obstacle obstaclePos 400
 
 handleKeys :: Event -> GameState -> GameState
-handleKeys (EventKey (SpecialKey KeyLeft) Down _ _) (player, obs, gen, t, status)
+handleKeys (EventKey (SpecialKey KeyLeft) Down _ _) (player, obs, gen, t, status, points)
     | status == Running && playerPosition player > (-windowWidth / 2 + 25) = 
-        (player { playerPosition = playerPosition player - playerSpeed * 10 }, obs, gen, t, Running)
-handleKeys (EventKey (SpecialKey KeyRight) Down _ _) (player, obs, gen, t, status) 
+        (player { playerPosition = playerPosition player - playerSpeed * 10 }, obs, gen, t, status, points)
+handleKeys (EventKey (SpecialKey KeyRight) Down _ _) (player, obs, gen, t, status, points) 
     | status == Running && playerPosition player < (windowWidth / 2 - 25) = 
-        (player { playerPosition = playerPosition player + playerSpeed * 10 }, obs, gen, t, Running)
-handleKeys (EventKey (Char ' ') Down _ _) (_, _, gen, _, GameOver) = 
+        (player { playerPosition = playerPosition player + playerSpeed * 10 }, obs, gen, t, status, points)
+handleKeys (EventKey (Char ' ') Down _ _) (_, _, gen, _, GameOver, points) = 
     initialState gen
 handleKeys _ s = s
+
 
 collides :: Player -> Obstacle -> Bool
 collides player obstacle = 
