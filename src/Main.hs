a0edadaeb8c0551f26b3bdca8500050fdc9aed46
analuizaff <nanaluff@hotmail.com>
2023-08-26 21:49:50 -0300

    corrigindo erro da velocidfade do obstaculo

--- src/Main.hs
+++ src/Main.hs
@@ -107,7 +107,7 @@ boldText x y c str =
 
 update :: Float -> GameState -> GameState
 update t state@(_, _, _, _, Menu, _) = state
-update t (player, obstacles, gen, time, status, score)
+update t (player, obstacles, gen, time, status, score) 
     | status == GameOver = (player, obstacles, gen, time, GameOver, score)
     | any (collisionDetected player) obstacles = (player, obstacles, gen, time, GameOver, score)
     | time > 1  = (player, newObstacle : movedObstacles, newGen, 0, Running, newScore)
@@ -118,10 +118,9 @@ update t (player, obstacles, gen, time, status, score)
 
     (obstaclePos, newGen) = randomR (-windowWidth / 2 + 25, windowWidth / 2 - 25) gen
     newObstacle = Obstacle obstaclePos 400
-
-    -- Adjusted the calculation of currentObstacleSpeed
-    currentObstacleSpeed = if score `mod` 10 == 0 then obstacleSpeed + fromIntegral (score `div` 10) * 3 else obstacleSpeed
+    
     newScore = score + 1
+    currentObstacleSpeed = if newScore `mod` 10 == 0 then obstacleSpeed + 3 else obstacleSpeed --aumenta a velocidade dos obstáculos em 3 a cada 10 pts
 
 
 
